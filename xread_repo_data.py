ebb96ecb5348498dd6d2fdaa1a3d7678043f118b
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2025-03-07 09:53:22 -0500

    Add files via upload

--- xread_repo_data.py
+++ xread_repo_data.py
@@ -1,10 +1,13 @@
+""" process repo data obtained by running xrepo_data.py,
+listing the most common topics """
 from github_util import read_repo_data, topics_to_repos
 from util import sort_dict_by_value_length
 
 nrepos_min = 1
+max_repos_print = 100
 print_dd = False
-infile = "fortran_repo_data.txt"
-print_repo_names = False
+infile = "fortran_repo_data.txt" # output of xrepo_data.py
+print_repo_names = True
 dd = read_repo_data(infile)
 if print_dd:
     for key, value in dd.items():
@@ -16,8 +19,10 @@ for key, value in dtopics.items():
     if nrepos < nrepos_min:
         break
     print("%5d"%nrepos, key)
-    if print_repo_names:
-        print(value, end="\n\n")
+    if print_repo_names and len(value) <= max_repos_print:
+        for x in value:
+            print(x)
+        print()
 
 
         
